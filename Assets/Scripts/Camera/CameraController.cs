293aae6d9d337d888dae188ef9c4ad15748f5fae
Pierrick FONQUERNE <pierrick.fonquerne@imaginativegames.co>
2023-04-11 23:42:13 +0200

    Update camera control

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 
+/// <summary>
+/// Controls the camera movement and rotation.
+/// </summary>
 public class CameraController : MonoBehaviour
 {
     public float moveSpeed;
     public float baseMoveSpeed = 5.0f;
     public float moveSpeedMultiplier = 1.0f;
-    public float rotationSpeed = 120.0f; // Rotation speed
-    public float rollSpeed = 60.0f; // Roll speed
-    public float yMinLimit = -80f; // Minimum vertical angle
-    public float yMaxLimit = 80f; // Maximum vertical angle
+    public float rotationSpeed = 120.0f;
+    public float rollSpeed = 60.0f;
+    public float yMinLimit = -80f;
+    public float yMaxLimit = 80f;
+    public float mouseSensitivity = 1.0f; // Mouse sensitivity
+    public float zoomSpeed = 5.0f; // Zoom speed
 
     private float x = 0.0f;
     private float y = 0.0f;
+    private bool isCameraLocked = false; // Camera lock
 
     void Start()
     {
@@ -22,37 +28,56 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        // Rotate the camera using the mouse
-        x += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-        y -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        // Toggle camera lock with the L key
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            isCameraLocked = !isCameraLocked;
+        }
 
-        y = ClampAngle(y, yMinLimit, yMaxLimit);
+        if (!isCameraLocked) // Only update camera rotation if it's not locked
+        {
+            // Rotate the camera using the mouse
+            x += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime * mouseSensitivity;
+            y -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime * mouseSensitivity;
 
-        Quaternion rotation = Quaternion.Euler(y, x, 0);
-        transform.rotation = rotation;
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            transform.rotation = rotation;
+        }
 
         // Calculate move speed based on distance from origin
         float distance = transform.position.magnitude;
         moveSpeed = baseMoveSpeed * (1.0f + distance * moveSpeedMultiplier);
 
-        // Move the camera using the ZQSD keys
+        // Move the camera using the arrow keys
         Vector3 moveDirection = new Vector3();
 
-        if (Input.GetKey(KeyCode.Z)) // Move forward
+        if (Input.GetKey(KeyCode.UpArrow)) // Move forward
         {
             moveDirection += transform.forward;
         }
-        if (Input.GetKey(KeyCode.S)) // Move backward
+        if (Input.GetKey(KeyCode.DownArrow)) // Move backward
         {
             moveDirection -= transform.forward;
         }
-        if (Input.GetKey(KeyCode.Q)) // Move left
+        if (Input.GetKey(KeyCode.LeftArrow)) // Move left
         {
             moveDirection -= transform.right;
         }
-        if (Input.GetKey(KeyCode.D)) // Move right
+        if (Input.GetKey(KeyCode.RightArrow)) // Move right
         {
             moveDirection += transform.right;
+        }
+
+        // Move the camera vertically using the Space and Ctrl keys
+        if (Input.GetKey(KeyCode.Space)) // Move up
+        {
+            moveDirection += transform.up;
+        }
+        if (Input.GetKey(KeyCode.LeftControl)) // Move down
+        {
+            moveDirection -= transform.up;
         }
 
         transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
@@ -66,9 +91,23 @@ public class CameraController : MonoBehaviour
         {
             transform.Rotate(Vector3.forward, -rollSpeed * Time.deltaTime, Space.Self);
         }
+
+        // New: Zoom in and out with the mouse wheel
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollInput != 0.0f)
+        {
+            float zoomAmount = scrollInput * zoomSpeed * Time.deltaTime;
+            transform.position += transform.forward * zoomAmount;
+        }
     }
 
-    // Clamp the angle between the min and max limits
+    /// <summary>
+    /// Clamp the angle between the min and max limits.
+    /// </summary>
+    /// <param name="angle">The angle to be clamped.</param>
+    /// <param name="min">The minimum limit of the angle.</param>
+    /// <param name="max">The maximum limit of the angle.</param>
+    /// <returns>Returns the clamped angle value.</returns>
     float ClampAngle(float angle, float min, float max)
     {
         if (angle < -360F)
